e561a65a33a258a62864d33bcddb32c51102e30d
joying <wengxianxun@hotmail.com>
2016-11-23 14:43:18 +0800

    sfd

---
+++ WxxKit/WxxKit.swift
@@ -0,0 +1,20 @@
+//
+//  WxxKit.swift
+//  WxxKit
+//
+//  Created by linxiaolong on 16/3/30.
+//  Copyright © 2016年 wengxianxun. All rights reserved.
+//
+
+import UIKit
+
+public class WxxKit: NSObject {
+    
+    public override init() {
+        print("init")
+    }
+    
+    public func doSomething(){
+        print("docomething")
+    }
+}
